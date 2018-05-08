98dfc35a0aac548f67bcc021dfeaf04f7462dc75
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-05-08 15:57:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,9 +45,7 @@ def turn_count(board)
 end #end turn_count
 
 def current_player(board)
-  player1 = "X"
-  player2 = "O"
-  turn_count(board).even? ? player1 : player2
+  turn_count(board).even? ? "X" : "O"
 end
 
 def play(board)
