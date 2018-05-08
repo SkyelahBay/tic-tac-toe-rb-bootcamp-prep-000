97d624eb69a52f61836011a33a7a3332beea0726
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-05-08 16:19:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,10 +56,12 @@ end
 def play(board)
   if !over?(board) #if false keep taking turns
     turn(board)
-  elsif won?(board) #if true, check for a winner
-    winner(board) == "X" ? "Congratulations X!" : "Congratulations O!" #if there is a winner but it wasn't X.
-  elsif draw?(board)
-    puts "Cat's game!"
+  elsif over?(board)
+    if won?(board) #if true, check for a winner
+      winner(board) == "X" ? "Congratulations X!" : "Congratulations O!" #if there is a winner but it wasn't X.
+    elsif draw?(board)
+      puts "Cat's game!"
+    end
   end
 end
 
